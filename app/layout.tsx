889ee7086f01c9ab5da2ea7477c3d87e17c2a8d3
Black4315 <yurytube4315@gmail.com>
2025-04-08 22:29:04 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono  ,Inter} from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Yahia's Portfolio",
+  description: "Modern & Minimal JS Mastery Portfolio",
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        suppressHydrationWarning
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import Approach from "@/components/Approach";
+import Clients from "@/components/clientsWord";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
+
+export default function Home() {
+  return (
+    <main role="main"className="relative flex-center flex-col mx-auto overflow-hidden sm:px-10 px-5 bg-black-100">
+      <div className="w-full max-w-7xl">
+        <FloatingNav navItems={navItems}/>
+        <Hero />
+        <Grid />
+        <Projects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+
+    </main>
+    
+  );
+}
