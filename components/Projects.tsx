34377396ef10a2cec6b6735a91cef0907ae9ddfc
Black4315 <yurytube4315@gmail.com>
2025-04-15 18:15:46 +0200

    yahia

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,7 +11,8 @@ const Projects = () => (
     <section className="py-20" id="projects">
         <h1 className="heading">
             A small selection of{" "}
-            <span className="text-purple">recent projects</span>
+            <span className="text-purple">my projects</span>
+            {/* recent */}
         </h1>
 
         <div className="flex-wrap flex-center p-4  sm:gap-16 mt-16">
@@ -20,7 +21,7 @@ const Projects = () => (
                     <PinContainer title={title} href={link}>
                         <div className="relative flex-center w-[80vw] sm:w-[430px] max-sm:h-[220px] mb-10 overflow-hidden">
                             <div className="relative w-full h-full overflow-hidden rounded-xl bg-[#13162d]">
-                                <img src="/bg.png" alt="bgimg" />
+                                <img src="/bg.png" loading="lazy" alt="bgimg" />
 
                             </div>
                             <Image src={img} width={370} height={370} alt={img} className="z-10 max-sm:w-[300px] absolute top-[28px] rotate-[2.16deg] rounded-[10px] object-cover" />
